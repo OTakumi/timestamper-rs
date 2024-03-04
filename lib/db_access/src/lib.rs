6f85164ddf3fefaca5e6ca7d0d5f4bdd10e13766
OTakumi <takumi.o.0410@gmail.com>
2024-03-04 18:18:35 +0900

    データベースへの接続機能を追加

--- lib/db_access/src/lib.rs
+++ lib/db_access/src/lib.rs
@@ -1,13 +1,43 @@
-struct DBAccess {
-    connection_str: String,
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+
+struct DBConnector {
+    db_name: &'static str,
+    db_host: &'static str,
+    db_user: &'static str,
+    db_password: &'static str,
 }
 
-impl DBAccess {
+#[allow(dead_code)]
+impl DBConnector {
     /// DB access constructor
-    fn new(hostname: &str) -> DBAccess {
-        DBAccess {
-            connection_str: format!("host={} user=postgres", hostname),
+    pub fn new(
+        database: &'static str,
+        host: &'static str,
+        user: &'static str,
+        password: &'static str,
+    ) -> DBConnector {
+        DBConnector {
+            db_name: database,
+            db_host: host,
+            db_user: user,
+            db_password: password,
         }
+    }
+
+    pub fn url(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}/{}",
+            self.db_user, self.db_password, self.db_host, self.db_name
+        )
+    }
+
+    pub fn establixh_connection(&self) -> PgConnection {
+        // DBへ接続する
+        let database_url = self.url();
+
+        PgConnection::establish(&database_url)
+            .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
     }
 }
 
@@ -16,8 +46,16 @@ mod tests {
     use super::*;
 
     #[test]
-    fn db_connection_string() {
-        let result = DBAccess::new("localhost");
-        assert_eq!(result.connection_str, "host=localhost user=postgres");
+    fn get_database_url() {
+        let postgres = DBConnector::new("diesel_demo", "localhost", "username", "password");
+        let db_url = postgres.url();
+        assert_eq!("postgres://username:password@localhost/diesel_demo", db_url);
+    }
+
+    #[test]
+    fn connect_to_database() {
+        let postgres = DBConnector::new("test", "localhost:5433", "postgres", "password");
+
+        postgres.establixh_connection();
     }
 }
